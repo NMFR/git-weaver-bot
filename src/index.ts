d548a33b7b373ad51219783dab442fb3686d4ad0
Nuno Rodrigues <nunomr@gmail.com>
2019-10-20 17:43:32 +0100

    Fix `npm run watch`
    `nodemon` was running `src/index.js` when `build/index.js` was not found and was listening to changes outside of `build/`.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.error("Not implemented yet!");
+console.error('Not implemented yet!');
+
+class TestClass {
+  integer: number;
+}
+
+function testFunc(str: string): string {
+  const x = new TestClass();
+  x.integer = 2;
+  return `test:${str} ${x.integer}`;
+}
+
+console.log(testFunc('hello world'));
